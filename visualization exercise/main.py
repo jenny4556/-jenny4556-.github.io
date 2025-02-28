2633c0d891801d711baf57f3db206a38f1f0b3f5
jenny4556 <klaudia.nurzynska@yahoo.com>
2025-02-28 18:40:20 +0100

    update 3

--- visualization exercise/main.py
+++ visualization exercise/main.py
@@ -3,6 +3,27 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+def plotImigrationsOverYeas():
+    #create a list of highest immigration rates to Canada over 1980-2014
+    df.sort_values(['Total'], ascending=False, axis=0, inplace=True)
+    top10 = df.drop(df[df["Country"] == "Total"].index, axis=0, inplace=True)
+    top10 = df['Country'].head(10)
+    print(top10)
+    years = list(map(int, range(1980, 2014))) 
+    df_selected = df[df["Country"].isin(top10)][["Country"] + years].set_index("Country")
+
+    # Transpose the data for plotting
+    df_selected = df_selected.T
+    df_selected.index = df_selected.index.astype(int)  # Convert years to int
+    plt.figure(figsize=(12, 6))
+    plt.stackplot(df_selected.index, df_selected.T, labels=df['Country'].head(10), alpha=0.7)
+
+    plt.xlabel("Year")
+    plt.ylabel("Number of Immigrants")
+    plt.title("Immigration to Canada (1980-2013)")
+    plt.legend(loc="upper left")
+    plt.grid(True)
+    plt.show()
 
 def plotImigrationFromACountry(country):
     #a func that takes a country name as a string and plots the distribution of imigration rates to Canada from theat country over years 1980-2013
@@ -17,7 +38,7 @@ def plotImigrationFromACountry(country):
 
     
 #data from https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DV0101EN-SkillsNetwork/Data%20Files/Canada.xlsx
-path = r"C:\Users\XC00355\Documents\Canada.xlsx"
+path = r"C:\Users\Klaudia\Documents\GitHub\-jenny4556-.github.io\visualization exercise\Canada.xlsx"
 
 xls = pd.ExcelFile(path)
 df = pd.read_excel(xls, sheet_name= 'Canada by Citizenship', skiprows=19)
@@ -45,15 +66,14 @@ yearsCols = df.iloc[:, 4:33]
 df['Total'] = yearsCols.sum(axis=1)
 #plotImigrationFromACountry('aaa')
 
-#create a list of highest immigration rates to Canada over 1980-2014
-top10 = df['Total'].sort_values(ascending=False)
+plotImigrationsOverYeas()
 
-top10.drop(columns = ['Continent', 'Region', 'DevName'], axis=1, inplace=True)
 
-yearsList = list(map(str, range(1980, 2013)))
-top102 = top10.transpose()
-top103 = df.head(10)
-print(top103)
+
+
+
+
+
 
 
 
